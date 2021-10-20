ac169dcff17c901aecce95d4777a2fa11ef6905c
Bhagaban Biswal <bhabiswa1@publicisgroupe.net>
2021-10-20 15:12:46 +0530

    Java8 prctice

---
+++ java-8/src/com/learnJava/streams/StreamsFilterMapReduceExample.java
@@ -0,0 +1,39 @@
+package com.learnJava.streams;
+
+import com.learnJava.data.Student;
+import com.learnJava.data.StudentDataBase;
+
+import java.util.function.Predicate;
+
+public class StreamsFilterMapReduceExample {
+
+    static Predicate<Student> genderPredicte =(student -> {
+        return student.getGender().equals("male");
+    });
+
+    static Predicate<Student> gradeLevel =(student -> {
+        return student.getGradeLevel()==2.0;
+    });
+
+    private static int noOfNoteBooks(){
+
+        int totalNoOfnoteBooks = StudentDataBase.getAllStudents().stream()
+                //.filter(genderPredicte)
+                .filter(gradeLevel)
+              /*  .peek(s->{
+                    System.out.println("s : "+ s);
+                })*/
+                .map((Student::getNoteBooks))
+               /* .peek(s1->{
+                    System.out.println("s1 : "+ s1);
+                })*/
+               // .reduce(0,(a,b)->a+b); //summing the notebooks.
+                .reduce(0,Integer::sum); //summing the notebooks.
+        return  totalNoOfnoteBooks;
+
+    }
+    public static void main(String[] args) {
+
+        System.out.println("Total No of NoteBooks are : " + noOfNoteBooks());
+    }
+}
